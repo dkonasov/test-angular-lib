805f54bf23f0eaa8e80f6ee7351916ff22011658
Konasov Dmitriy <d.konasov@yandex.ru>
2019-12-10 15:46:25 +0300

    addeed component selector

--- src/components/test-lib/test-lib.component.ts
+++ src/components/test-lib/test-lib.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-    templateUrl: './test-lib.component.html'
+    templateUrl: './test-lib.component.html',
+    selector: 'tlib-test-component'
 })
 export class TestLibComponent {
     title = 'Test lib test component'
